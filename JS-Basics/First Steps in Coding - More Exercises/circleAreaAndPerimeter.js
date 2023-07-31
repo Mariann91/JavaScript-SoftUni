4a05fe9619b46cb333abc3c01068f697d3da25a4
Mariann91 <125202659+Mariann91@users.noreply.github.com>
2023-07-31 12:17:56 +0300

    Create circleAreaAndPerimeter.js

---
+++ JS-Basics/First Steps in Coding - More Exercises/circleAreaAndPerimeter.js
@@ -0,0 +1,8 @@
+function paintingHouse(input) {
+  let radius = input[0];
+  let s = Math.PI * radius ** 2;
+  let p = 2 * Math.PI * radius;
+  
+  console.log(s.toFixed(2));
+  console.log(p.toFixed(2));
+}
