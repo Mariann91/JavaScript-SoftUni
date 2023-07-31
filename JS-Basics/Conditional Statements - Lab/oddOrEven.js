e48b6dd65ce954832b491b5fa5758cb72e554d9b
Mariann91 <125202659+Mariann91@users.noreply.github.com>
2023-07-31 13:54:30 +0300

    Create oddOrEven.js

---
+++ JS-Basics/Conditional Statements - Lab/oddOrEven.js
@@ -0,0 +1,11 @@
+function oddOrEven(input) {
+  let inputNumber = Number(input[0]);
+  
+  if (inputNumber % 2 !== 0) {
+    console.log("odd")
+    
+  } else {
+    console.log("even")
+  }
+  
+}
