dfaf823b3d397e30de64ce44c30965b90465a45c
Mariann91 <125202659+Mariann91@users.noreply.github.com>
2023-08-01 10:13:33 +0300

    Create time15Minutes.js

---
+++ JS-Basics/Conditional Statements - Exercise/time15Minutes.js
@@ -0,0 +1,20 @@
+function time15Minutes(input) {
+  let hours = Number(input[0]);
+  let minutes = Number(input[1]) + 15;
+  
+  if (minutes > 59) {
+    minutes -= 60;
+    hours += 1;
+  }
+  
+  if (hours > 23) {
+    hours -= 24;
+  }
+  
+  if (minutes < 10) {
+    console.log(`${hours}:0${minutes}`)
+  } else {
+    console.log(`${hours}:${minutes}`)
+  }
+ 
+}
