8e985bd20dc186210f7c19ff2a5945874fcc857d
Mariann91 <125202659+Mariann91@users.noreply.github.com>
2023-08-02 19:09:35 +0300

    Create personalTitles.js

---
+++ JS-Basics/Conditional Statements Advanced - Lab/personalTitles.js
@@ -0,0 +1,26 @@
+function personalTitles(input) {
+    let age = Number(input[0]);
+    let gender = input[1];
+
+    let result = "";
+
+    switch (gender) {
+        case "m":
+            if (age < 16) {
+                result = "Master";
+            } else {
+                result = "Mr."
+            }
+            break;
+        case "f":
+            if (age < 16) {
+                result = "Miss";
+            } else {
+                result = "Ms."
+            }
+            break;
+    }
+
+    console.log(result);
+
+}
