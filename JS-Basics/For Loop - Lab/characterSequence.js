403890f37eeee33506470329da32abf2ac23551e
Mariann91 <125202659+Mariann91@users.noreply.github.com>
2023-08-04 12:21:52 +0300

    Create characterSequence.js

---
+++ JS-Basics/For Loop - Lab/characterSequence.js
@@ -0,0 +1,9 @@
+function characterSequence(input) {
+  let word = input[0];
+  let wordLength = word.length;
+  
+  for (let index = 0; index < wordLength; index++) {
+    console.log(word[index]);
+  }
+  
+}
