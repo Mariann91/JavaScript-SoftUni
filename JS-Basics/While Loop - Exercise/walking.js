4cbe87b0427f743dd75db77091ebe6a3429c1cab
Mariann91 <125202659+Mariann91@users.noreply.github.com>
2023-08-06 21:06:17 +0300

    Create walking.js

---
+++ JS-Basics/While Loop - Exercise/walking.js
@@ -0,0 +1,44 @@
+function walking(input) {
+
+    let targetSteps = 10_000;
+    
+    let index = 0;
+    let walkedSteps = 0;
+    
+
+    while (true) {
+        let currentSteps = input[index];
+        console.log(currentSteps);
+
+        if (currentSteps === "Going home") {
+            let stepsToHome = Number(input[index + 1]);
+            walkedSteps += stepsToHome;
+            let difference = Math.abs(walkedSteps - targetSteps);
+            
+            if (walkedSteps >= targetSteps) {
+                console.log("Goal reached! Good job!");
+                console.log(`${difference} steps over the goal!`);
+                break;
+
+            } else {
+                console.log(`${difference} more steps to reach goal.`);
+                break;
+            }
+            
+        }
+
+        currentSteps = Number(currentSteps);
+        console.log(currentSteps);
+        walkedSteps += currentSteps;
+        
+        if (walkedSteps >= targetSteps) {
+            let difference = Math.abs(walkedSteps - targetSteps);
+            console.log("Goal reached! Good job!");
+            console.log(`${difference} steps over the goal!`);
+            break;
+        }
+
+        index++;
+
+    }
+}
