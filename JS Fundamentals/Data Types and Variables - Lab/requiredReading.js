a4aa00050dc259c818a20633ce787e0652f40236
Mariann91 <125202659+Mariann91@users.noreply.github.com>
2023-08-09 13:23:10 +0300

    Create requiredReading.js

---
+++ JS Fundamentals/Data Types and Variables - Lab/requiredReading.js
@@ -0,0 +1,6 @@
+function requiredReading(bookPages, pagesPerHour, daysToRead) {
+  let pagesPerDay = bookPages / daysToRead;
+  let hoursPerDay = pagesPerDay / pagesPerHour;
+  
+  console.log(hoursPerDay)
+}
