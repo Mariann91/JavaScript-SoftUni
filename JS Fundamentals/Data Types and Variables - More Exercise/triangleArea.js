82612acd7865045d51bbbb9eb599a5d5a2ba0747
Mariann91 <125202659+Mariann91@users.noreply.github.com>
2023-08-10 20:54:40 +0300

    Create triangleArea.js

---
+++ JS Fundamentals/Data Types and Variables - More Exercise/triangleArea.js
@@ -0,0 +1,7 @@
+function triangleArea(side1, side2, side3) {
+   let semiPerimeter = 0.5 * (side1 + side2 + side3);
+   let area = Math.sqrt(semiPerimeter * (semiPerimeter - side1) * (semiPerimeter - side2) * (semiPerimeter - side3))
+   
+   console.log(area);
+
+}
