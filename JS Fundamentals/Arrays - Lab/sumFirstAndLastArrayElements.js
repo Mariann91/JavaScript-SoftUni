17cf49a83dd4af14c4d3ada954080b0525525c6b
Mariann91 <125202659+Mariann91@users.noreply.github.com>
2023-08-11 08:07:50 +0300

    Create sumFirstAndLastArrayElements.js

---
+++ JS Fundamentals/Arrays - Lab/sumFirstAndLastArrayElements.js
@@ -0,0 +1,7 @@
+function sumFirstAndLastArrayElements(array) {
+  let firstElement = array[0];
+  let lastElement = array[array.length - 1];
+  let sumElements = firstElement + lastElement;
+  
+  console.log(sumElements);
+}
