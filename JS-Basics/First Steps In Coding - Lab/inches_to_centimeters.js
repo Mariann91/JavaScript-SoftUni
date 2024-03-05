ee5c3d30cfa2151052fa0f0b29c98ea5be134755
Mariann91 <125202659+Mariann91@users.noreply.github.com>
2024-03-05 09:01:56 +0200

    Update inches_to_centimeters.js

--- JS-Basics/First Steps In Coding - Lab/inches_to_centimeters.js
+++ JS-Basics/First Steps In Coding - Lab/inches_to_centimeters.js
@@ -1,6 +1,7 @@
-function inchesToSm(input) {
-  let inches = Number(input[0]);
-  let inch = 2.54;
-  let sm = inches * inch;
-  console.log(sm)
+function inchesToCentimeters(santimeters){
+  const inch = 2.54;
+  
+  let result = santimeters * inch;
+  
+  console.log(result);
 }
