d4a5422873bc05ed1e5d6f5bb16f302faacd3118
Mariann91 <125202659+Mariann91@users.noreply.github.com>
2024-03-07 10:24:24 +0200

    Update workingHours.js

--- JS-Basics/Conditional Statements Advanced - Lab/workingHours.js
+++ JS-Basics/Conditional Statements Advanced - Lab/workingHours.js
@@ -1,23 +1,20 @@
-function workingHours(input) {
-    let hour = Number(input[0]);
-    let day = input[1];
-
-    switch (day) {
-        case "Monday":
-        case "Tuesday":
-        case "Wednesday":
-        case "Thursday":
-        case "Friday":
-        case "Saturday":
-            if (hour >= 10 && hour <= 18){
-                console.log("open")
-            } else {
-                console.log("closed")
-            }
-            break;
-        case "Sunday":
-            console.log("closed")
-            break;
-    }
-
+function workingHours(data) {
+  let hour = Number(data[0]);
+  let day = data[1];
+  
+  let open = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    ]
+  if (open.includes(day) && hour >= 10 && hour <= 18) {
+    
+    console.log('open');
+  } else {
+    console.log("closed");
+  }
+  
 }
