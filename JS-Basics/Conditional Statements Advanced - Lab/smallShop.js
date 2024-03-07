669dc3a0a83bf6c5a89f5d9add4ecb45547eef1a
Mariann91 <125202659+Mariann91@users.noreply.github.com>
2024-03-07 09:36:23 +0200

    Update smallShop.js

--- JS-Basics/Conditional Statements Advanced - Lab/smallShop.js
+++ JS-Basics/Conditional Statements Advanced - Lab/smallShop.js
@@ -1,74 +1,33 @@
-function smallShop(input) {
-    let product = input[0];
-    let city = input[1];
-    let quantity = input[2];
+function smallShop(data) {
+  let product = data[0];
+  let city = data[1];
+  let quantity = Number(data[2]);
+  
+  let finalPrice;
+  
+  
+  let productPrices = {
+    "coffee": [0.50, 0.40, 0.45],
+    "water": [0.80, 0.70, 0.70],
+    "beer": [1.20, 1.15, 1.10],
+    "sweets": [1.45, 1.30, 1.35],
+    "peanuts": [1.60, 1.50, 1.55],
+  }
 
-    let charge = 0;
+  switch (city) {
+    case 'Sofia':
+      finalPrice = productPrices[product][0] * quantity;
+      break;
+    case 'Plovdiv':
+      finalPrice = productPrices[product][1] * quantity;
+      break
+    
+    default:
+      finalPrice = productPrices[product][2] * quantity;
+      break;
+      // code
+  }
+  
+  console.log(finalPrice);
 
-    switch (city) {
-        case "Sofia":
-
-            switch (product){
-                case "coffee":
-                    charge = quantity * 0.50;
-                    break;
-                case "water":
-                    charge = quantity * 0.80;
-                    break;
-                case "beer":
-                    charge = quantity * 1.20;
-                    break;
-                case "sweets":
-                    charge = quantity * 1.45;
-                    break;
-                case "peanuts":
-                    charge = quantity * 1.60;
-                    break;
-            }
-            break;
-        case "Plovdiv":
-            switch (product){
-
-                case "coffee":
-                    charge = quantity * 0.40;
-                    break;
-                case "water":
-                    charge = quantity * 0.70;
-                    break;
-                case "beer":
-                    charge = quantity * 1.15;
-                    break;
-                case "sweets":
-                    charge = quantity * 1.30;
-                    break;
-                case "peanuts":
-                    charge = quantity * 1.50;
-                    break;
-            
-            }
-            break;
-
-            case "Varna":
-                switch (product){
-
-                    case "coffee":
-                        charge = quantity * 0.45;
-                        break;
-                    case "water":
-                        charge = quantity * 0.70;
-                        break;
-                    case "beer":
-                        charge = quantity * 1.10;
-                        break;
-                    case "sweets":
-                        charge = quantity * 1.35;
-                        break;
-                    case "peanuts":
-                        charge = quantity * 1.55;
-                        break;              
-                }
-                break;
-    }
-
-    console.log(charge);
 }
