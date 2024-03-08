4f242cd7fafe90f7d1b8a06790b45cc0e020d2a7
Mariann91 <125202659+Mariann91@users.noreply.github.com>
2024-03-08 11:33:32 +0200

    Update tennisRanklist.js

--- JS-Basics/For Loop - Exercise/tennisRanklist.js
+++ JS-Basics/For Loop - Exercise/tennisRanklist.js
@@ -1,32 +1,28 @@
-function tennisRanklist(input) {
-    let tournamentsCount = Number(input[0]);
-    let startingPoints = Number(input[1]);
-
-    let wonTournaments = 0;
-    let tournamentsPoints = 0;
-
-    for (let i = 2; i < input.length; i++) {
-        let round = input[i];
-
-        switch (round) {
-            case "W":
-                wonTournaments++;
-                tournamentsPoints += 2000;
-                break;
-            case "F":
-                tournamentsPoints += 1200;
-                break;
-            case "SF":
-                tournamentsPoints += 720;
-                break;
-        }
-    }
-
-    let finalPoints = startingPoints + tournamentsPoints;
-    let averagePoints = Math.floor(tournamentsPoints / tournamentsCount);
-    let wonTournamentsPercent = wonTournaments / tournamentsCount * 100;
-
-    console.log(`Final points: ${finalPoints}`);
-    console.log(`Average points: ${averagePoints}`);
-    console.log(`${wonTournamentsPercent.toFixed(2)}%`)
+function tennisRanklist(data) {
+  let tournaments = Number(data[0]);
+  let startingPoints = Number(data[1]);
+  let wonPoints = 0;
+  let wins = 0;
+  
+  let points = {
+    'W': 2000,
+    'F': 1200,
+    'SF': 720,
+  }
+  
+  for (i=2; i <= data.length - 1; i++) {
+    let currentTournamentPoints = points[data[i]]
+    
+    wonPoints += currentTournamentPoints;
+    
+    if (currentTournamentPoints === 2000) {
+      wins += 1;
+    }  
+  }
+  
+  let finalPoints = startingPoints + wonPoints;
+  
+  console.log(`Final points: ${finalPoints}`);
+  console.log(`Average points: ${Math.floor(wonPoints / tournaments)}`)
+  console.log(`${((wins / tournaments) * 100).toFixed(2)}%`)
 }
