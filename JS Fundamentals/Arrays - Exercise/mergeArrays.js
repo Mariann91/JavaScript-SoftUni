9d87e2c6d54ff09b21800677e956976ec188a024
Mariann91 <125202659+Mariann91@users.noreply.github.com>
2024-03-29 10:36:45 +0200

    Update mergeArrays.js

--- JS Fundamentals/Arrays - Exercise/mergeArrays.js
+++ JS Fundamentals/Arrays - Exercise/mergeArrays.js
@@ -1,19 +1,14 @@
-function mergeArrays(array1, array2) {
-   let mergedArray = []
-
-   for (let index = 0; index < array1.length; index++) {
-      let array1CurrentItem = array1[index];
-      let array2CurrentItem = array2[index];
-      let itemToPush
-
-      if (index % 2 === 0) {
-         itemToPush = Number(array1CurrentItem) + Number(array2CurrentItem);
-      } else {
-         itemToPush = array1CurrentItem + array2CurrentItem;
-      }
-
-      mergedArray.push(itemToPush);
-   }
-
-   console.log(mergedArray.join(" - "));
+function solve(array1, array2) {
+  let newArray = [];
+  
+  for (let index = 0; index < array1.length; index++) {
+    
+    if (index % 2 == 0) {
+      newArray.push(Number(array1[index]) + Number(array2[index]));
+    } else {
+      newArray.push(array1[index] + array2[index]);
+    }
+    
+  }
+  console.log(newArray.join(" - "))
 }
